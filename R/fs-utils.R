480efac22f1a4dff81c3cb69f6ae8f806742bd3c
Vince Reuter <vince.reuter@gmail.com>
2017-06-29 10:39:34 -0400

    bring over filesystem path expansion function, add doc, remove function dependency and include NA case; line length

---
+++ R/fs-utils.R
@@ -0,0 +1,37 @@
+# fs-utils.R
+# Author: Vince Reuter
+# email: vince.reuter@gmail.com
+
+
+#' Path expansion utility
+#'
+#' \code{ExpandPath} takes a path and expands user/environment variables.
+#' \code{NULL}, \code{NA}, and empty path inputs are returned as-is. For 
+#' environment variables, the assumption is that each will be prefixed with 
+#' a dollar-sign \code{$}.
+#'
+#' @param path The filesystem path to expand.
+#' @return Input path with user/environment variables expanded.
+#' @export
+ExpandPath = function(path) {
+
+  # Handle null/empty input.
+  if (is.null(path) | is.na(path) | identical("", path)) { return(path) }
+
+  # Helper functions
+  chopPath = function(p) { 
+    if (p == dirname(p)) p else c(chopPath(dirname(p)), basename(p)) }
+  expand = function(pathPart) { 
+    if (startsWith(pathPart, "$")) {
+      system(sprintf("echo %s", pathPart), intern = TRUE)
+    } else { pathPart }
+
+  # Split path; short-circuit return or ensure no reference to this folder.
+  parts = chopPath(path)
+  if (length(parts) < 2) { return(parts) }
+  if (identical(".", parts[1])) { parts = parts[2:length(parts)] }
+
+  # Expand any environment variables and return the complete path.
+  fullPath = do.call(file.path, lapply(parts, expand))
+  return(fullPath)
+}
