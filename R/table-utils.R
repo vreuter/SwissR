0042b7377e7c56b8611d7a6d0eed55f8a1b784fe
Vince Reuter <vince.reuter@gmail.com>
2017-09-01 21:36:18 -0400

    testing TODO

--- R/table-utils.R
+++ R/table-utils.R
@@ -12,6 +12,11 @@
 #'           unique value of \code{column}.
 #' @export
 nOfEach = function(dt, column, n) {
+  # TODO: how/what to test:
+  # 1. Empty table.
+  # 2. Missing column
+  # 3. Value(s) with fewer than n observations.
+  # 4. Universal quantification on nrow (should match #(unique(column))).
   if (n < 1) { stop("Choose to take at least one of each value") }
   vals = dt[, get(column)]
   indexer = sapply( X=unique(vals), FUN=function(v) { which(v == vals)[1:n] } )
@@ -30,5 +35,10 @@ nOfEach = function(dt, column, n) {
 #' @seealso \code{\link{nOfEach}}
 #' @export
 oneOfEach = function(...) {
+  # TODO: how/what to test:
+  # 0. Empty table
+  # 0.5. Missing column
+  # 1. duplicate 'n' argument --> error.
+  # 2. no duplicates for any of the unique values of the column chosen (universal).
   nOfEach(..., n=1)
 }
