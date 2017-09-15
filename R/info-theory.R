27876ba6e14d219d98fea03ec95a300d805d5ba1
Vince Reuter <vince.reuter@gmail.com>
2017-09-15 12:32:54 -0400

    allow counts rather than raw observations as argument to entropy functions

--- R/info-theory.R
+++ R/info-theory.R
@@ -11,17 +11,27 @@
 #' The base two logarithm is used, so the unit for the result is bits.
 #'
 #' @param observations The observed values for which to calculate entropy.
+#' @param isCounts Does \code{observations} consist of observation counts 
+#'               rather than actual observations?
+#' @param pseudocount Value of pseudocount to substitute for zeros; this has 
+#'                    meaning if and only if \code{counts=TRUE}.
 #' @param normalized Whether to normalize the entropy (into [0, 1]).
 #' @return The Shannon entropy of the empirical distribution defined by 
 #'         the given observations.
 #' @export
-shannonEntropy = function(observations, normalized=FALSE) {
-  # Handle non-factor input.
-  if (!is.factor(observations)) {
-    observations = factor(observations, levels=unique(observations))
+shannonEntropy = function(observations, 
+  isCounts=FALSE, pseudocount=1, normalized=FALSE) {
+  
+  if (isCounts) { counts = insertPseudocounts(observations, pseudocount) }
+  else {
+    # Handle non-factor input.
+    if (!is.factor(observations)) {
+      observations = factor(observations, levels=unique(observations))
+    }
+    num_obs = length(observations)
+    counts = sapply(X=levels(observations), FUN=function(x) sum(x == observations))
   }
-  num_obs = length(observations)
-  counts = sapply(X=levels(observations), FUN=function(x) sum(x == observations))
+  
   probs = sapply(X=counts, FUN=function(n) n/num_obs)
   # We're protected from zero-probability cases since we've defined the 
   # domain to be the set of unique observations.
